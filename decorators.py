e43c6396e11614081c7fd4c6bb4a438f2e6ae5e1
alexishh670 <53016172+alexishh670@users.noreply.github.com>
2019-07-31 19:19:48 -0500

    clase de decoradores

---
+++ decorators.py
@@ -0,0 +1,32 @@
+PASSWORD = '12345'
+
+def password_required(func):
+     def wrapper():
+         password = input('cual es tu contrasena')
+
+         if password == PASSWORD:
+             return func()
+
+         else:
+             print('la contraseña no es correcta')
+    
+     return wrapper
+
+@password_required
+def needs_password():
+    print('la contraseña es correcta')
+
+
+def upper(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+
+        return result.upper()
+    return wrapper
+
+@upper
+def say_my_name(name):
+    return 'hola, {}'.format(name)
+
+if __name__== '__main__':
+    print(say_my_name('alexis'))
