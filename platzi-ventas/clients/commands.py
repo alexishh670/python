0678666c19b897ff354498f6ee8c3028631086b7
alexishh670 <53016172+alexishh670@users.noreply.github.com>
2019-08-02 17:11:43 -0500

    dqw

--- platzi-ventas/clients/commands.py
+++ platzi-ventas/clients/commands.py
@@ -1,5 +1,5 @@
 import click
-
+ 
 @click.group()
 def clients():
     """Manages the clients lifecycle"""
@@ -7,28 +7,28 @@ def clients():
 
 
 @clients.command()
-@click.pass_context()
+@click.pass_context
 def create(ctx, name, company, email, position):
     """creates a new client"""
     pass
 
 
 @clients.command()
-@click.pass_context()
+@click.pass_context
 def list(ctx):
     """List all clients"""
     pass
 
 
 @clients.command()
-@click.pass_context()
+@click.pass_context
 def update(ctx, client_uid):
     """Updates a client"""
     pass
 
 
 @clients.command()
-@click.pass_context()
+@click.pass_context
 def delete(ctx, client_uid):
     """Deletes a client"""
     pass
